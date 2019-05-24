77013c57eca5826f0e28de86c5f308706059f2ec
Brad Curran <brad@bradleycurran.com>
2019-05-23 17:45:20 -0700

    Init

---
+++ generators/app/templates/gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const { spawn } = require('child_process');
+
+const allSrc = ['src/**/*.js', 'gulpfile.js'];
+
+const watchFiles = ['src/**/*.js', 'gulpfile.js', 'testfile.json'];
+
+let nodeProcess;
+
+const clear = done => {
+  process.stdout.write('\u001B[2J\u001B[0;0f');
+  done();
+};
+
+const lint = () => {
+  return gulp
+    .src(allSrc)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+};
+
+const runNode = done => {
+  if (nodeProcess) {
+    nodeProcess.kill();
+  }
+
+  nodeProcess = spawn('node', ['src'], { stdio: 'inherit' });
+
+  nodeProcess.on('close', code => {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes');
+    }
+  });
+
+  done();
+};
+
+const watch = () => {
+  return gulp.watch(watchFiles, gulp.series(clear, lint, runNode));
+};
+
+gulp.task('default', gulp.series(clear, lint, runNode, watch));
